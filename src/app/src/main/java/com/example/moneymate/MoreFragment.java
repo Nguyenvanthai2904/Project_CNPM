d194530fd185ada6674e0f59688ccc59f8b274bb
dai2410 <ngod5481@gmail.com>
2024-10-31 14:04:11 +0700

    .

--- src/app/src/main/java/com/example/moneymate/MoreFragment.java
+++ src/app/src/main/java/com/example/moneymate/MoreFragment.java
@@ -7,36 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MoreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class MoreFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    int image[] = {R.drawable.caidat, R.drawable.thaydoimau, R.drawable.baocao, R.drawable.dangxuat};
+    String name[] = {"Cài đặt", "Thay đổi màu", "Báo cáo", "Đăng xuất"};
+    ArrayList<item> mylist;
+    MoreMyArrayAdapter moremyarrrayadapter;
+    ListView lv;
 
     public MoreFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MoreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MoreFragment newInstance(String param1, String param2) {
         MoreFragment fragment = new MoreFragment();
         Bundle args = new Bundle();
@@ -49,9 +40,16 @@ public class MoreFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        // Initialize the list of items
+        mylist = new ArrayList<>();
+        for (int i = 0; i < name.length; i++) {
+            mylist.add(new item(image[i], name[i]));
         }
     }
 
@@ -59,6 +57,13 @@ public class MoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+
+        // Initialize the ListView and set up the adapter here
+        lv = view.findViewById(R.id.lv);
+        moremyarrrayadapter = new MoreMyArrayAdapter(getActivity(), R.layout.layout_item, mylist);
+        lv.setAdapter(moremyarrrayadapter);
+
+        return view;
     }
 }
---
+++ src/app/src/main/java/com/example/moneymate/MoreMyArrayAdapter.java
@@ -0,0 +1,48 @@
+package com.example.moneymate;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import android.widget.TextView;
+public class MoreMyArrayAdapter extends ArrayAdapter<item>{
+    Activity context;
+    int Idlayout;
+    ArrayList<item> mylist;
+    // Tạo cóntructor
+
+    public MoreMyArrayAdapter( Activity context, int idlayout, ArrayList<item> mylist) {
+        super(context, idlayout, mylist);
+        this.context = context;
+        Idlayout = idlayout;
+        this.mylist = mylist;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater myflacter = context.getLayoutInflater();
+        // Đặt idlayout lên để tạo thành đối tượng view
+        convertView = myflacter.inflate(Idlayout, null);
+        // lấy phần tử trong mảng;
+        item myitem = mylist.get(position);
+        // khai báo tham chiếu Id và hiển thịk ảnh lên ImageView
+        ImageView imgcaidat = convertView.findViewById(R.id.imgcaidat);
+        imgcaidat.setImageResource(myitem.getImage());
+        //khai báo, tham chiếu id và hiển thị tên điện thoại lên text view
+        TextView txtcaidat = convertView.findViewById(R.id.txtcaidat);
+        txtcaidat.setText(myitem.getName());
+        return convertView;
+    }
+
+}
